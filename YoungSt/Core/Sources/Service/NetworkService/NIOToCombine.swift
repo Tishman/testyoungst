a0bad1029cff29c5f3c43c86ca93340d1112db0b
Tischenko Roman <>
2021-03-14 21:05:01 +0300

    Add NIO Extension

---
+++ YoungSt/Core/Sources/Service/NetworkService/NIOToCombine.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Роман Тищенко on 14.03.2021.
+//
+
+import Combine
+import NIO
+
+public extension NIO.EventLoopFuture {
+    var publisher: Future<Value, Error> {
+        Future { promise in
+            self.whenComplete { result in
+                promise(result)
+            }
+        }
+    }
+}
