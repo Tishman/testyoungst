7513b643057fc054037104c4515fc371c7715712
Tischenko Roman <>
2021-05-02 10:39:45 +0600

    [VYOUNGST-T-84] Add files

---
+++ YoungSt/Core/Sources/Domain/Authorization/ForgotPassword/ForgotPasswordModels.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Роман Тищенко on 02.05.2021.
+//
+
+import Foundation
---
+++ YoungSt/Core/Sources/Domain/Authorization/ForgotPassword/ForgotPasswordScene.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Роман Тищенко on 02.05.2021.
+//
+
+import SwiftUI
+
+struct ForgotPasswordScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForgotPasswordScene_Previews: PreviewProvider {
+    static var previews: some View {
+		ForgotPasswordScene()
+    }
+}
